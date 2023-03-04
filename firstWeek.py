62e15e3d8710ec32a62549d7145aab0533972768
mahmoudessam16 <mahmoudemourad29@gmail.com>
2023-03-04 15:00:22 +0200

    Python Assignments

---
+++ firstWeek.py
@@ -0,0 +1,26 @@
+# Assignment One
+# this is my first file in python
+# this file have a function to print my name
+print("Mahmoud Essam")
+
+
+# Assignment Two
+name = "Mahmoud"
+age = "23"
+country = "Egypt"
+
+
+# Assignment Three
+print("Name: " + name)
+print("Age: " + age)
+print("Country: " + country)
+
+
+# Assignment Four
+print("Hello, My Name Is " + name + " And Iam " +
+      age + " Years Old and I Live in " + country)
+
+# Assignment Five
+print(type(name))
+print(type(age))
+print(type(country))
